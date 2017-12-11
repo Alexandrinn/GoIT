2f61c9df56c8baa950c11c1f07a0c5b0460e24a7
OlhaGitRepo <hlibovich.olga@gmail.com>
2017-12-11 09:28:45 +0200

    Added star

--- src/Lessons/Lesson1/Main.java
+++ src/Lessons/Lesson1/Main.java
@@ -35,8 +35,8 @@ public class Main {
 //        ---------------
 
     public static void FirTree () {
-        System.out.println("      /\\\n     /  \\\n    / || \\\n   /  ||  \\\n  /   ||   \\\n /    ||    \\\n" +
-                "/     ||     \\\n      ||\n--------------");
+        System.out.println("\n       *\n       ^\n      / \\\n     /   \\\n    / | | \\\n   /  | |  \\\n  /   | |   \\\n /    | |    \\\n" +
+                "/     | |     \\\n      | |\n---------------");
     }
 
 //        Задание 3
